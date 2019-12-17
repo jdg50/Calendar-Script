6ad2fd624a34a34fd668e63dc6680d627c8c36bf
jdg50 <58829287+jdg50@users.noreply.github.com>
2019-12-17 09:15:09 -0600

    Update Calendar.ps1

--- Calendar.ps1
+++ Calendar.ps1
@@ -1,2 +1,9 @@
-﻿$O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $O365Cred -Authentication Basic -AllowRedirection
-Import-PSSession $o365Session
+﻿<# $O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $O365Cred -Authentication Basic -AllowRedirection
+Import-PSSession $o365Session #>
+
+Write-Host "What calendar would you like to change permissions for?
+1. For HTX Conference Rooms
+2. For ATX Conference Rooms
+3. For every Conference room
+4. For a specific user
+"
