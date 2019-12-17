1c3f4b86f317933cedb1e531db8b2e2ee9c33d7d
jdg50 <58829287+jdg50@users.noreply.github.com>
2019-12-17 09:41:34 -0600

    Update Calendar.ps1

--- Calendar.ps1
+++ Calendar.ps1
@@ -1,16 +1,16 @@
 ﻿$O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $O365Cred -Authentication Basic -AllowRedirection
 Import-PSSession $o365Session
 
-$input = Read-Host "Enter the email of the user that you are adding to the calendar: "
-Get-Mailbox $input
+$userinput = Read-Host "Enter the email of the user that you are adding to the calendar: "
+Get-Mailbox $userinput
 do {
-$Input = Read-Host "What calendar would you like to change permissions for?
+$ChoiceInput = Read-Host "What calendar would you like to change permissions for?
 1. For HTX Conference Rooms
 2. For ATX Conference Rooms
 3. For every Conference room
 4. For a specific user"
-Switch ($input) {
-1 {}
+Switch ($Choiceinput) {
+1 {Add-MailboxFolderPermission CCR@trioltd.com:\calendar -user $userinput -accessrights Editor}
 2 {}
 3 {}
 4 {}
