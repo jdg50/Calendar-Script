1d67b4f46f059e1e43571a4e7c96e1fe58754812
jdg50 <58829287+jdg50@users.noreply.github.com>
2020-03-16 12:34:10 -0500

    Update Calendar.ps1

--- Calendar.ps1
+++ Calendar.ps1
@@ -7,10 +7,18 @@ $HTXConference = "CCR@trioltd.com:\calendar", "FCR@trioltd.com:\calendar", "LRC@
 $ATXConference = "ATXCONF@trioltd.com:\calendar", "ATXCONF2@trioltd.com:\calendar"
 $AllConference = $HTXConference + $ATXConference
 
+#Prompt the user what they would like to do:
+do {
+$StartInput = Read-host "Enter what you would like to do:
+1. Add Calendar Permissions
+2. Remove Calendar Permissions
+3. Check Calendar Permissions"
+Switch ($StartInput) {
+1 {
 #Prompt the user to enter the email of the user being given calendar permissions
 #Note that after every switch statement, the connection from exchange is removed via Remove-PSSession
-$userinput = Read-Host "Enter the email of the user that you are adding to the calendar: "
 do {
+$userinput = Read-Host "Enter the email of the user that you are adding to the calendar: "
 $ChoiceInput = Read-Host "What calendar would you like to change permissions for?
 1. For HTX Conference Rooms
 2. For ATX Conference Rooms
@@ -19,31 +27,37 @@ $ChoiceInput = Read-Host "What calendar would you like to change permissions for
 Switch ($Choiceinput) {
 1 {
    foreach ($HTXConf in $HTXConference) {
-   Add-MailboxFolderPermission $HTXConf -user $userinput -accessrights Editor}
+   Add-MailboxFolderPermission $HTXConf -user $userinput -accessrights Editor
+   Set-CalendarProcessing $HTXConf -ResourceDelegates $userinput}
    Remove-Pssession $O365Session
    }
 2 {
    foreach ($ATXConf in $ATXConference) {
-   Add-MailboxFolderPermission $ATXConf -user $userinput -accessrights Editor}
+   Add-MailboxFolderPermission $ATXConf -user $userinput -accessrights Editor
+   Set-CalendarProcessing $ATXConf -ResourceDelegates $userinput}
    Remove-Pssession $O365Session
    }
 3 {
    foreach ($Allconf in $Allconference) {
-   Add-MailboxFolderPermission $AllConf -user $userinput -accessrights Editor}
+   Add-MailboxFolderPermission $AllConf -user $userinput -accessrights Editor
+   Set-CalendarProcessing $AllConf -ResourceDelegates $userinput}
    Remove-Pssession $O365Session
    }
-4 {$userchoice = "Other"}
-Default {continue}
-}
-}
-While ($input -ne 1 -and $input -ne 2 -and $input -ne 3 -and $input -ne 4)
-
-#Prompts the user to enter the email of who they would like to give $userinput access to
-#Also removes the exchange connection at the end
-if ($userchoice -eq "Other") {
+4 {
 $userchoice = Read-host "Enter the email of the user you would like to give" $userinput "access to."
 $userchoice = $userchoice + ":\calendar"
 $userrights = Read-Host "Enter the access you would like the user to have: (Owner/Editor/Reviewer/AvailabilityOnly/LimitedDetails)"
 Add-MailboxFolderpermission $userchoice -user $userinput -accessrights $userrights
 Remove-Pssession $O365Session
 }
+Default {continue}
+}
+}
+While ($input -ne 1 -and $input -ne 2 -and $input -ne 3 -and $input -ne 4)
+}
+2 {Write-Host "Input 2"}
+3 {Write-Host "Input 3"}
+Default {continue}
+}
+}
+while ($input -ne 1 -and $input -ne 2 -and $input -ne 3)
