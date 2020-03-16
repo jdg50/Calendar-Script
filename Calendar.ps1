3ade034290c892dd506b84a0b4d3fb8f5a1b6b97
jdg50 <58829287+jdg50@users.noreply.github.com>
2020-03-16 09:29:46 -0500

    Update Calendar.ps1

--- Calendar.ps1
+++ Calendar.ps1
@@ -1,8 +1,11 @@
 ﻿#Connect to exchange powershell. Not MSonline, exchange. 
 $O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $O365Cred -Authentication Basic -AllowRedirection
 Import-PSSession $o365Session
+
+#Put conference room titles in an array
 $HTXConference = "CCR@trioltd.com:\calendar", "FCR@trioltd.com:\calendar", "LRC@trioltd.com:\calendar", "SCR@trioltd.com:\calendar"
 $ATXConference = "ATXCONF@trioltd.com:\calendar", "ATXCONF2@trioltd.com:\calendar"
+$AllConference = $HTXConference + $ATXConference
 
 #Prompt the user to enter the email of the user being given calendar permissions
 #Note that after every switch statement, the connection from exchange is removed via Remove-PSSession
@@ -14,22 +17,19 @@ $ChoiceInput = Read-Host "What calendar would you like to change permissions for
 3. For every Conference room
 4. For a specific user"
 Switch ($Choiceinput) {
-1 {Add-MailboxFolderPermission CCR@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission FCR@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission LRC@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission SCR@trioltd.com:\calendar -user $userinput -accessrights Editor
+1 {
+   foreach ($HTXConf in $HTXConference) {
+   Add-MailboxFolderPermission $HTXConf -user $userinput -accessrights Editor}
    Remove-Pssession $O365Session
    }
-2 {Add-MailboxFolderPermission ATXCONF@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission ATXCONF2@trioltd.com:\calendar -user $userinput -accessrights Editor
+2 {
+   foreach ($ATXConf in $ATXConference) {
+   Add-MailboxFolderPermission $ATXConf -user $userinput -accessrights Editor}
    Remove-Pssession $O365Session
    }
-3 {Add-MailboxFolderPermission CCR@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission FCR@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission LRC@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission SCR@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission ATXCONF@trioltd.com:\calendar -user $userinput -accessrights Editor
-   Add-MailboxFolderPermission ATXCONF2@trioltd.com:\calendar -user $userinput -accessrights Editor
+3 {
+   foreach ($Allconf in $Allconference) {
+   Add-MailboxFolderPermission $AllConf -user $userinput -accessrights Editor}
    Remove-Pssession $O365Session
    }
 4 {$userchoice = "Other"}
